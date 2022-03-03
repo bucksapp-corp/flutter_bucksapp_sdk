de35541dcbbf656fed28fb3980b8217d577135cc
Raul <raul@bucksapp.co>
2022-03-03 10:04:55 -0300

    BB-385 SDK de WebApp para Flutter (mobile y web)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,115 +1,284 @@
+import 'dart:io';
+import 'package:example/widgets/icons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bucksapp_sdk/flutter_bucksapp_sdk.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:flutter_svg/svg.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isAndroid) {
+    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
+
+    var swAvailable = await AndroidWebViewFeature.isFeatureSupported(
+        AndroidWebViewFeature.SERVICE_WORKER_BASIC_USAGE);
+    var swInterceptAvailable = await AndroidWebViewFeature.isFeatureSupported(
+        AndroidWebViewFeature.SERVICE_WORKER_SHOULD_INTERCEPT_REQUEST);
+
+    if (swAvailable && swInterceptAvailable) {
+      AndroidServiceWorkerController serviceWorkerController =
+          AndroidServiceWorkerController.instance();
+
+      serviceWorkerController.serviceWorkerClient = AndroidServiceWorkerClient(
+        shouldInterceptRequest: (request) async {
+          return null;
+        },
+      );
+    }
+  }
+
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Demo',
+      supportedLocales: const [
+        Locale('en', ''),
+        Locale('es', ''),
+      ],
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          primarySwatch: const MaterialColor(0xFF2C5597, <int, Color>{
+        50: Color(0xFFE6EBF3),
+        100: Color(0xFFC0CCE0),
+        200: Color(0xFF96AACB),
+        300: Color(0xFF6B88B6),
+        400: Color(0xFF4C6FA7),
+        500: Color(0xFF2C5597),
+        600: Color(0xFF274E8F),
+        700: Color(0xFF214484),
+        800: Color(0xFF1B3B7A),
+        900: Color(0xFF102A69),
+      })),
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => const MyHomePage(title: 'Demo'),
+        '/simulator': (BuildContext context) => const Simulator(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
+  final String title;
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  final text = '''
+¡Bienvenido a la demo de BucksApp! 
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+Esta app le demuestrá cómo nuestra herramienta puede lucir integrada a su aplicación bancaria. 
 
-  final String title;
+(Los datos utilizados son solo de ejemplo)
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+''';
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        title: Text(
+          title,
+          style: const TextStyle(color: Colors.white),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Stack(
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 64.0, horizontal: 16.0),
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "Lorem Ipsum Dolot Est",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+                  ),
+                ),
+                Flex(
+                  direction: Axis.horizontal,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    const Expanded(
+                      child: Card(
+                        color: Colors.blue,
+                        child: Padding(
+                          padding: EdgeInsets.all(48.0),
+                          child: Text(''),
+                        ),
+                      ),
+                    ),
+                    const Expanded(
+                      child: Card(
+                        color: Colors.blue,
+                        child: Padding(
+                          padding: EdgeInsets.all(48.0),
+                          child: Text(''),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Flex(
+                  direction: Axis.horizontal,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    const Expanded(
+                      child: Card(
+                        color: Colors.blue,
+                        child: Padding(
+                          padding: EdgeInsets.all(48.0),
+                          child: Text(''),
+                        ),
+                      ),
+                    ),
+                    const Expanded(
+                      child: Card(
+                        color: Colors.blue,
+                        child: Padding(
+                          padding: EdgeInsets.all(48.0),
+                          child: Text(''),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(
+            color: const Color(0xE5E5E5FF),
+            child: Center(
+                child: Padding(
+              padding: const EdgeInsets.all(64.0),
+              child: Card(
+                color: Theme.of(context).primaryColor,
+                elevation: 20,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Flex(
+                  direction: Axis.vertical,
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SvgPicture.asset('icons/Logo.svg'),
+                          const Text(
+                            "DEMO",
+                            style: TextStyle(color: Colors.white),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              bottomRight: Radius.circular(20),
+                              bottomLeft: Radius.circular(20)),
+                          color: Colors.white),
+                      child: Padding(
+                        padding: const EdgeInsets.all(19.0),
+                        child: Text(
+                          text,
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )),
+          ),
+          const Positioned(bottom: 0, right: 32, child: Clip())
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed, // This is all you need!
+        onTap: (int index) {
+          if (index == 3) Navigator.of(context).pushNamed('/simulator');
+        },
+
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(UiIcons.housing),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(UiIcons.transfer), label: 'Transferir'),
+          BottomNavigationBarItem(
+              icon: Icon(UiIcons.cashTransaction), label: 'Pagar'),
+          BottomNavigationBarItem(
+              icon: Icon(UiIcons.financials), label: 'Mis Finanzas'),
+        ],
+      ),
+    );
+  }
+}
+
+class Simulator extends StatelessWidget {
+  const Simulator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Theme.of(context).primaryColor,
+      child: SafeArea(
+        child: LayoutBuilder(builder: (context, constraints) {
+          return Scaffold(
+            backgroundColor: Theme.of(context).primaryColor,
+            body: SingleChildScrollView(
+              child: SizedBox(
+                  height: constraints.maxHeight,
+                  width: constraints.maxWidth,
+                  child: Bucksapp(
+                    apiKey: dotenv.get('API_KEY', fallback: ''),
+                    uuid: dotenv.get('UUID', fallback: ''),
+                  )),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+class Clip extends StatelessWidget {
+  const Clip({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SvgPicture.asset(
+          'icons/clip.svg',
+        ),
+        const SizedBox(
+          width: 150,
+          height: 80,
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 24.0),
+            child: Text(
+              'Tocá acá para empezar',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 14),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
--- lib/flutter_bucksapp_sdk.dart
+++ lib/flutter_bucksapp_sdk.dart
@@ -1,7 +1,87 @@
 library flutter_bucksapp_sdk;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:http/http.dart' as http;
+
+class Bucksapp extends StatefulWidget {
+  const Bucksapp({
+    Key? key,
+    required this.apiKey,
+    required this.uuid,
+  }) : super(key: key);
+
+  final String apiKey;
+  final String uuid;
+
+  @override
+  State<Bucksapp> createState() => _BucksappState();
+}
+
+class _BucksappState extends State<Bucksapp> {
+  late InAppWebViewController webView;
+  final CookieManager cookieManager = CookieManager.instance();
+
+  Future<String?> getToken() async {
+    final response = await http.post(
+        Uri.parse('https://api.dev.bucksapp.com/api/fi/v1/authenticate'),
+        headers: {
+          'jwt_aud': 'development',
+          'Content-Type': 'application/json',
+          'X-API-KEY': widget.apiKey
+        },
+        body: json.encode({"user": widget.uuid}));
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body)?["token"];
+    } else {
+      return response.reasonPhrase;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<String?>(
+        future: getToken(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            cookieManager.setCookie(
+                url: Uri.parse('https://app.dev.bucksapp.com'),
+                name: "token",
+                value: snapshot.data!);
+            cookieManager.setCookie(
+                url: Uri.parse('https://app.dev.bucksapp.com'),
+                name: "NEXT_LOCALE",
+                value: "es");
+
+            return InAppWebView(
+              initialOptions: InAppWebViewGroupOptions(
+                android: AndroidInAppWebViewOptions(useHybridComposition: true),
+              ),
+              initialUrlRequest: URLRequest(
+                url: Uri.parse('https://app.dev.bucksapp.com'),
+                method: 'GET',
+                headers: {
+                  HttpHeaders.authorizationHeader: 'Bearer ${snapshot.data}'
+                },
+              ),
+              onWebViewCreated: (InAppWebViewController controller) {
+                controller.addJavaScriptHandler(
+                    handlerName: 'handlerBack',
+                    callback: (args) {
+                      Navigator.of(context).pop();
+                    });
+                webView = controller;
+              },
+            );
+          }
+          return const Center(
+              child: CircularProgressIndicator(
+            color: Colors.white,
+          ));
+        });
+  }
 }
